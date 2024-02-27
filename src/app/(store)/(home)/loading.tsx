05bb14b0d18bd878d1167a15b6f177a65164e456
Gabriel Viana <viannadoze@gmail.com>
2024-02-27 15:20:05 -0300

    Loading na Home

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,12 @@
+import { Skeleton } from "@/components/skeleton"
+
 export default function HomeLoading() {
-    return <p>Carregando...</p>
+    return (
+        <div className="grid h-full grid-cols-9 grid-rows gap-6">
+            <Skeleton className="col-span-6 row-span-6 n-[760px]"/>
+            <Skeleton className="col-span-3 row-span-3 n-[760px]"/>
+            <Skeleton className="col-span-3 row-span-3 n-[760px]"/>
+        </div>
+    )
+
 }
