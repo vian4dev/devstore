06e00728d4e9336c0b178f885daed93597025bb5
Gabriel Viana <viannadoze@gmail.com>
2024-03-30 23:04:29 -0300

    Formulário de busca

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { SearchIcon, User } from "lucide-react";
-import Image from "next/image";
+import { User } from "lucide-react";
 import { CartWidget } from "./cart-widget";
+import { SearchForm } from "./search-form";
 
 export function Header() {
     return (
@@ -12,11 +12,7 @@ export function Header() {
                     devstore
                 </Link>
 
-                <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-                    <SearchIcon className="h-5 w-5 text-zinc-500" />
-
-                    <input placeholder="Buscar produto..." className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500" />
-                </form>
+                <SearchForm />
             </div>
 
             <div className="flex items-center gap-4">
